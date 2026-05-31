2844dd1bdfe35fd81acea354877d35566736b798
yarabadra71-dr <yarabadra71@gmail.com>
2026-05-31 19:32:49 +0300

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,20 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const getRouter = () => {
+
+export const router = (() => {
+  const queryClient = new QueryClient();
+
+  const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+
+  return router;
+};
+
   const queryClient = new QueryClient();
 
   const router = createRouter({
