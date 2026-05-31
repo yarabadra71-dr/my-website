4b1a4107744f8236ab0da5796a2a9b040df90178
yarabadra71-dr <yarabadra71@gmail.com>
2026-05-31 20:09:46 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
-  component: Root,
+  component: () => {
+    return (
+      <>
+        <Outlet />
+      </>
+    )
+  },
 })
-
-function Root() {
-  return <Outlet />
-}
